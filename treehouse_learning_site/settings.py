ca09426341436c28c0dd0b55ba161f7247a6d295
Kyle Chivers <chiverskyle@gmail.com>
2017-10-12 18:35:24 -0700

    updated template directory

--- treehouse_learning_site/settings.py
+++ treehouse_learning_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'treehouse_learning_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
