a9e161b3e3ddb71cf45cd9bc6d81921f1eba6426
Kyle Chivers <chiverskyle@gmail.com>
2017-10-15 13:58:16 -0700

    404 handling added to view

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Course
 
@@ -13,5 +13,5 @@ def course_list(request):
 	return render(request, 'courses/course_list.html', {'courses':courses})
 
 def course_detail(request, pk):
-	course = Course.objects.get(pk=pk)
+	course = get_object_or_404(Course, pk=pk)
 	return render(request, 'courses/course_detail.html', {'course':course})
