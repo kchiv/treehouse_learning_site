73c34448828647db527ae4460d7b756417a2f56f
Kyle Chivers <chiverskyle@gmail.com>
2017-10-15 11:07:15 -0700

    added detail view to app

--- courses/views.py
+++ courses/views.py
@@ -11,3 +11,7 @@ from .models import Course
 def course_list(request):
 	courses = Course.objects.all()
 	return render(request, 'courses/course_list.html', {'courses':courses})
+
+def course_detail(request, pk):
+	course = Course.objects.get(pk=pk)
+	return render(request, 'courses/course_detail.html', {'course':course})
