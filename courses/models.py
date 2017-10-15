24c0cf232ed896c776eb76e88dd3edeed4f53bcf
Kyle Chivers <chiverskyle@gmail.com>
2017-10-15 13:39:52 -0700

    meta class added to Step model

--- courses/models.py
+++ courses/models.py
@@ -19,6 +19,9 @@ class Step(models.Model):
 	order = models.IntegerField(default=0)
 	course = models.ForeignKey(Course)
 
+	class Meta:
+		ordering = ['order',]
+
 	def __str__(self):
 		return self.title
 
