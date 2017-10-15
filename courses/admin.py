9bbf64ec07730a5dabfec79b0190bde9b330c1d7
Kyle Chivers <chiverskyle@gmail.com>
2017-10-14 19:50:38 -0700

    registered step

--- courses/admin.py
+++ courses/admin.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Course
+from .models import Course, Step
 
 # Register your models here.
 
 admin.site.register(Course)
+admin.site.register(Step)
