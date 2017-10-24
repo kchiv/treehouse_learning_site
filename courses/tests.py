bd4df8629777fa43facedd190a24fe36b8f40ef8
Kyle Chivers <chiverskyle@gmail.com>
2017-10-23 22:17:09 -0700

    added model test

--- courses/tests.py
+++ courses/tests.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Course
 
 # Create your tests here.
+
+class CourseModelTests(TestCase):
+	def test_course_creation(self):
+		course = Course.objects.create(
+			title = 'Python Regular Expressions',
+			description = 'Learn to write regular expressions in Python'
+		)
+		now = timezone.now()
+		self.assertLess(course.created_at, now)
