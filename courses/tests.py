0f611d3589449a609f0779eb886c1629e7945f7f
Kyle Chivers <kchiv@kyles-mbp.lan>
2017-10-24 20:35:08 -0700

    added view test

--- courses/tests.py
+++ courses/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
@@ -16,3 +17,26 @@ class CourseModelTests(TestCase):
 		)
 		now = timezone.now()
 		self.assertLess(course.created_at, now)
+
+
+class CourseViewsTests(TestCase):
+	def setup(self):
+		self.course = Course.objects.create(
+			title = 'Python Testing',
+			description = 'Learn to write tests in Python.'
+		)
+		self.course2 = Course.objects.create(
+			title = 'New Course',
+			description = 'A new course.'
+		)
+		self.step = Step.objects.create(
+			title = 'Introduction to Doctests',
+			description = 'Learn to write tests in your docstrings.',
+			course = self.course
+		)
+
+	def test_course_list_view(self):
+		resp = self.client.get(reverse('courses:list'))
+		self.assertEqual(resp.status_code, 200)
+		self.assertIn(self.course, resp.context['courses'])
+		self.assertIn(self.course2, resp.context['courses'])
