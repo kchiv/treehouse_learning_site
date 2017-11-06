c72e0ea1a7c73195ddec47e3f97fda76150c19a0
Kyle Chivers <kchiv@kyles-mbp.lan>
2017-11-05 20:54:47 -0800

    custom template tag method added

---
+++ courses/templatetags/course_extras.py
@@ -0,0 +1,12 @@
+from django import template
+
+from courses.models import Course
+
+register = template.Library()
+
+@register.simple_tag
+def newest_course():
+	''' Gets the most recent course.'''
+	return Course.objects.latest('created_at')
+
+
