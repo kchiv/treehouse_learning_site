3fe40fd71c41de858418aca99458512f22b56db6
Kyle Chivers <kchiv@kyles-mbp.lan>
2017-11-10 23:14:35 -0800

    add template filter

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -1,5 +1,7 @@
 from django import template
 
+import markdown2
+
 from courses.models import Course
 
 register = template.Library()
@@ -14,3 +16,11 @@ def nav_courses_list():
 	'''Returns dictionary of courses to display as navigation pane'''
 	courses = Course.objects.all()
 	return {'courses': courses}
+
+@register.filter('time_estimate')
+def time_estimate(word_count):
+	'''Estimates the number of minutes it will take to complete a step
+	based on the passed in word count'''
+	minutes = round(word_count/20)
+	return minutes
+
