38157625acf375e280ecaf31e04fd16d0ffd2128
Kyle Chivers <kchiv@kyles-mbp.lan>
2017-11-22 21:55:56 -0800

    new models added

--- courses/admin.py
+++ courses/admin.py
@@ -2,16 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Course, Text, Quiz
+from . import models
 
 # Register your models here.
 
-class TextInline(admin.StackedInline):
-	model = Text
-
-class CourseAdmin(admin.ModelAdmin):
-	inlines = [TextInline,]
-
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Text)
-admin.site.register(Quiz)
+admin.site.register(models.Course)
+admin.site.register(models.Text)
+admin.site.register(models.Quiz)
+admin.site.register(models.Question)
+admin.site.register(models.Answer)
