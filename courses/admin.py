ebffa5b97ed5558c2b7bbd08f8cc1e7ea035b5bb
Kyle Chivers <kchiv@kyles-mbp.lan>
2017-11-22 16:10:24 -0800

    added abstract inheritance

--- courses/admin.py
+++ courses/admin.py
@@ -2,15 +2,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Course, Step
+from .models import Course, Text
 
 # Register your models here.
 
-class StepInline(admin.StackedInline):
-	model = Step
+class TextInline(admin.StackedInline):
+	model = Text
 
 class CourseAdmin(admin.ModelAdmin):
-	inlines = [StepInline,]
+	inlines = [TextInline,]
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Text)
 
