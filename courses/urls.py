93ea291ae175172b71f3b51bc9d5b41f0d85bace
Kyle Chivers <kchiv@kyles-mbp.lan>
2017-12-02 12:30:37 -0800

    added routing for question editing

--- courses/urls.py
+++ courses/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
 	url(r'(?P<course_pk>\d+)/create_quiz/$', views.quiz_create, name='create_quiz'),
 	url(r'(?P<course_pk>\d+)/edit_quiz/(?P<quiz_pk>\d+)/$', views.quiz_edit, name='edit_quiz'),
 	url(r'(?P<quiz_pk>\d+)/create_question/(?P<question_type>mc|tf)/$', views.create_question, name='create_question'),
+	url(r'(?P<quiz_pk>\d+)/edit_question/(?P<question_pk>\d+)/$', views.edit_question, name='edit_question'),
 	url(r'(?P<pk>\d+)/$', views.course_detail, name='detail'),
 ]
