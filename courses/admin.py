bed9803ec8e4aacbd90f992bfe7a6cbcae269b8e
Kyle Chivers <kchiv@kyles-mbp.lan>
2018-01-07 10:32:56 -0800

    included more customizations for admin

--- courses/admin.py
+++ courses/admin.py
@@ -14,7 +14,7 @@ class QuizInline(admin.StackedInline):
     model = models.Quiz
 
 
-class AnswerInline(admin.StackedInline):
+class AnswerInline(admin.TabularInline):
     model = models.Answer
 
 class YearListFilter(admin.SimpleListFilter):
@@ -39,18 +39,29 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['title', 'description']
     list_filter = ['created_at', YearListFilter]
     list_display = ['title', 'created_at', 'published', 'time_to_complete']
+    list_editable = ['published']
 
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [AnswerInline,]
     search_fields = ['prompt']
+    list_display = ['prompt', 'quiz', 'order']
+    list_editable = ['quiz', 'order']
+    radio_fields = {'quiz': admin.VERTICAL}
 
 class QuizAdmin(admin.ModelAdmin):
 	fields = ['course', 'title', 'description', 'order', 'total_questions']
 
+class TextAdmin(admin.ModelAdmin):
+	#fields = ['course', 'title', 'order', 'description', 'content']
+	fieldsets = (
+		('Details', {'fields': ('course', 'title', 'order', 'description')}),
+		('Add content', {'fields': ('content',), 'classes': ('collapse',)})
+	)
+
 
 admin.site.register(models.Course, CourseAdmin)
-admin.site.register(models.Text)
+admin.site.register(models.Text, TextAdmin)
 admin.site.register(models.Quiz, QuizAdmin)
 admin.site.register(models.MultipleChoiceQuestion, QuestionAdmin)
 admin.site.register(models.TrueFalseQuestion, QuestionAdmin)
